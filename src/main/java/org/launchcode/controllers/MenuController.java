c49d5735a3d6d083fc2bce902573be9c880107f4
neex-io <neex.io@protonmail.com>
2017-09-18 15:04:38 -0700

    Implement adding new menus

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -20,4 +20,24 @@ public class MenuController {
         model.addAttribute("title", "Categories");
         return "menu/index";
     }
+
+    @RequestMapping(value="add", method=RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Menu());
+        model.addAttribute("title", "Add Menu");
+        return "menu/add";
+    }
+
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors) {
+
+        // check for errors as always
+        if (errors.hasErrors()) {
+            return "menu/add";
+        } // implicit else
+
+        menuDao.save(menu);
+        return "redirect:/menu/view/" + menu.getId();
+    }
+
 }
