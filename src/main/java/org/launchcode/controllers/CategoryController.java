7c35337af4325721f9a1c2c940fd4fee6367e466
neex-io <neex.io@protonmail.com>
2017-09-18 12:14:05 -0700

    Add handler for add in Category

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
@@ -21,5 +24,24 @@ public class CategoryController {
         model.addAttribute("categories", categoryDao.findAll());
         model.addAttribute("title", "Categories");
         return "category/index";
+    }
+
+    @RequestMapping(value = "add", method= RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add category");
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Category category,
+                      Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "category/add";
+        } // implicit else
+
+        categoryDao.save(category);
+        return "redirect:";
     }
 }
