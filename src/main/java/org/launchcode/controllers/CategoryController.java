ec63f7d2198132a811c29f9f00a19c4c0e45a40c
neex-io <neex.io@protonmail.com>
2017-09-18 12:13:44 -0700

    Add handler for index in Category

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,9 +1,13 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("category")
@@ -11,4 +15,11 @@ public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "Categories");
+        return "category/index";
+    }
 }
